b099f29d220bf418a2ab09802b8c32f3bef33887
RAFA ATH ZANAR <zanarmhmra.21@uom.lk>
2024-09-10 20:16:54 +0530

    playera commit

---
+++ playera_backend/src/main/java/com/zanar/playera/PlayeraApplication.java
@@ -0,0 +1,13 @@
+package com.zanar.playera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayeraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayeraApplication.class, args);
+	}
+
+}
